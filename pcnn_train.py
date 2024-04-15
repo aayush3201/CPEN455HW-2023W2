7f0190c9ef7b9b21a6bd4c57e3a2ae600d212f95
aayushbehl <aayush.behl@ubc.ca>
2024-04-15 14:47:13 -0700

    Add labels to sampling step

--- pcnn_train.py
+++ pcnn_train.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
         
         if epoch % args.sampling_interval == 0:
             print('......sampling......')
-            sample_t = sample(model, args.sample_batch_size, args.obs, sample_op)
+            sample_t = sample(model, labels_t, args.sample_batch_size, args.obs, sample_op)
             sample_t = rescaling_inv(sample_t)
             save_images(sample_t, args.sample_dir)
             sample_result = wandb.Image(sample_t, caption="epoch {}".format(epoch))
