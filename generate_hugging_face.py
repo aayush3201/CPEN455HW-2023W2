dfa14a6f2dace203fb9ae23a556ff480fe1b8c37
aayushbehl <aayush.behl@ubc.ca>
2024-04-16 23:31:15 -0700

    Add FID to csv

--- generate_hugging_face.py
+++ generate_hugging_face.py
@@ -1,13 +1,26 @@
+'''
+This file is to be used to test the model at models/conditional_pixelcnn.pth
+and generate all the necessary files.
+'''
 from torchvision import datasets, transforms
 from utils import *
 from model import * 
 from dataset import *
 from tqdm import tqdm
 from pprint import pprint
+from classification_evaluation import get_label
 import argparse
+import os
+from pytorch_fid.fid_score import calculate_fid_given_paths
+from generation_evaluation import my_sample
 NUM_CLASSES = len(my_bidict)
 
 if __name__ == '__main__':
+    ref_data_dir = "data/test"
+    gen_data_dir = "samples"
+    BATCH_SIZE=128
+    if not os.path.exists(gen_data_dir):
+        os.makedirs(gen_data_dir)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     kwargs = {'num_workers':0, 'pin_memory':True, 'drop_last':False}
     ds_transforms = transforms.Compose([transforms.Resize((32, 32)), rescaling])
@@ -15,7 +28,7 @@ if __name__ == '__main__':
                                                             mode = 'test', 
                                                             transform=ds_transforms, include_paths=True), 
                                              batch_size=32, 
-                                             shuffle=True, 
+                                             shuffle=False, 
                                              **kwargs)
 
     model = PixelCNN(nr_resnet=1, nr_filters=40, nr_logistic_mix=5, input_channels=3)
@@ -23,14 +36,19 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load('models/conditional_pixelcnn.pth'))
     model.eval()
     print('model parameters loaded')
-    hugging_csv = 'id, label\n' 
+    hugging_csv = 'id,label\n' 
     for batch_idx, item in enumerate(tqdm(dataloader)):
         model_input, categories, img_path = item
         model_input = model_input.to(device)
         answer = get_label(model, model_input, device)
         for i in range(len(answer)):
-            hugging_csv = hugging_csv + f"{img_path[i].split('/').[-1]},{int(answer[i])}\n"
-    hugging_csv = hugging_csv + 'fid, 455'
+            hugging_csv = hugging_csv + f"{img_path[i].split('/')[-1]},{int(answer[i])}\n"
+
+    paths = [gen_data_dir, ref_data_dir]
+    print("Begin sampling!")
+    my_sample(model=model, gen_data_dir=gen_data_dir)
+    fid_score = calculate_fid_given_paths(paths, BATCH_SIZE, device, dims=192)
+    hugging_csv = hugging_csv + f"fid,{fid_score}"
 
     f = open("hugging_face.csv", "w")
     f.write(hugging_csv)
